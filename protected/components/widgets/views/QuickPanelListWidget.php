d30eb8fe25920e4edf61d3c83dfe2434935d19e1
dmitry <dmitry123@inbox.ru>
2015-03-23 16:17:11 -0700

    Widgets folder removed from component folder

--- protected/components/widgets/views/QuickPanelListWidget.php
+++
@@ -1,5 +0,0 @@
-<?php foreach($icons as $icon) { ?>
-    <a href="<?php echo $icon['href']; ?>">
-        <img src="<?php echo $icon['icon']; ?>" alt="" width="50" height="50" title="" />
-    </a>
-<?php } ?>
