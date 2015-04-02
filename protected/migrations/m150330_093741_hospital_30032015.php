24029cf3e8391235e5b12005d422cf63c5b5279b
dmitry <dmitry123@inbox.ru>
2015-04-02 12:56:47 -0700

    Hospital migration fixes

--- protected/migrations/m150330_093741_hospital_30032015.php
+++ protected/migrations/m150330_093741_hospital_30032015.php
@@ -1,19 +1,13 @@
 <?php
 
 class m150330_093741_hospital_30032015 extends CDbMigration {
-	public function up() {
-        $this->getDbConnection()->createCommand(
-            "INSERT INTO mis.access_actions_groups (id, name) VALUES(9, 'Стационар')"
-        )->execute();
 
-        $this->getDbConnection()->createCommand(
-            "INSERT INTO mis.access_actions (\"name\", \"group\", \"accessKey\") VALUES('Видимость модуля в меню', 9, 'hospitalMenu')"
-        )->execute();
+	public function safeUp() {
+		$this->execute("INSERT INTO mis.access_actions_groups (id, name) VALUES(9, 'Стационар')");
+		$this->execute("INSERT INTO mis.access_actions (\"name\", \"group\", \"accessKey\") VALUES('Видимость модуля в меню', 9, 'hospitalMenu')");
 	}
 
-	public function down() {
-        $this->getDbConnection()->createCommand(
-            "DELETE FROM mis.access_action_groups WHERE id = 9"
-        )->execute();
+	public function safeDown() {
+		$this->execute("DELETE FROM mis.access_actions_groups WHERE id = 9");
 	}
 }
