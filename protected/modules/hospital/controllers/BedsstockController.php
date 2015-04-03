521b5c5818aa8b0f09793dfa855d07848e26828e
Cls <kkosorukov@atlant-inform.ru>
2015-04-03 17:05:14 +0300

    Fx

--- protected/modules/hospital/controllers/BedsstockController.php
+++ protected/modules/hospital/controllers/BedsstockController.php
@@ -1,5 +1,5 @@
 <?php 
-class BedsStockController extends Controller {
+class BedsstockController extends Controller {
 	public $layout = 'application.modules.hospital.views.layouts.bedsstock';
 	public function actionView() {
 		$this->render('index', array(
