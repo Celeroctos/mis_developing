0b961e15b514bc3a3b4187b605f822c340fb7c47
dmitry <dmitry123@inbox.ru>
2015-07-16 16:56:10 +0300

    Fixed error reporting

--- index.php
+++ index.php
@@ -1,22 +1,18 @@
 <?php
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
 ob_start();
 // change the following paths if necessary
 $yii1Path = '/../yii';
 $yii1 = dirname(__FILE__).$yii1Path.'/yii.php';
-
 $yii2Path = '/../yii2/';
 $yii2 = dirname(__FILE__).$yii2Path.'/framework/yii.php';
-
 $configYii1 = dirname(__FILE__).'/protected/config/main.php';
 //$configYii2 = dirname(__FILE__).'/protected/config/main_yii2.php';
-
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
 require_once("../yii/yii.php");
 //require_once($yii2);
-
 //new yii\web\Application($yii2Config);
 Yii::createWebApplication($configYii1)->run();
