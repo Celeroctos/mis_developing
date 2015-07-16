b067f0e2f11835b53fa1981346d47fb64232f07b
dmitry <dmitry123@inbox.ru>
2015-07-16 16:41:55 +0300

    Asset bundles fixes

--- protected/assets/CoreAsset.php
+++ protected/assets/CoreAsset.php
@@ -4,16 +4,24 @@ class CoreAsset extends AssetBundle {
 
 	public $js = [
 		"js/core/core.js",
+		"js/core/panel.js",
+		"js/core/loading.js",
 		"js/core/form.js",
 		"js/core/multiple.js",
 		"js/core/message.js",
 		"js/core/address.js",
+		"js/core/table.js",
 	];
 
 	public $css = [
 		"css/message.css",
-		"css/multiple.css"
+		"css/panel.css",
+		"css/multiple.css",
+		"css/loading.css",
+		"css/table.css",
 	];
 
-	public $dependencies = [];
+	public $dependencies = [
+		"DefaultAsset"
+	];
 }
