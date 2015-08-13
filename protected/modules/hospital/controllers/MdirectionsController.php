9cae91815e97b5514747b27a0015697b457bce48
hushker <hushker@gmail.com>
2015-08-13 13:10:39 +0300

    case sensitive problem on linux

---
+++ protected/modules/hospital/controllers/MdirectionsController.php
@@ -0,0 +1,66 @@
+<?php
+class MdirectionsController extends Controller {
+    public function actionAdd() {
+        if(!isset($_POST['FormDirectionForPatientAdd'])) {
+            exit(CJSON::encode(
+                array(
+                    'success' => false
+                )
+            ));
+        }
+
+        $model = new FormDirectionForPatientAdd();
+        //var_dump($_POST['FormDirectionForPatientAdd']);
+        $model->attributes = $_POST['FormDirectionForPatientAdd'];
+        if(!$model->validate()) {
+            exit(CJSON::encode(
+                array(
+                    'success' => false,
+                    'errors' => $model->errors
+                )
+            ));
+        }
+
+        $oms = Oms::model()->findByPk($model->omsId);
+        if(!$oms) {
+            throw new Exception('Нет ОМС с ID = '.$model->omsId);
+        }
+
+        // Create patient from data, if not exists
+        $patientModel = new Patient();
+        $patient = $patientModel->getPatient($model, $oms);
+        // Create medical direction for patient
+        $mDirection = new MDirection();
+        $mDirection = $mDirection->create($patient, $model);
+
+        echo CJSON::encode(array(
+            'success' => true
+        ));
+    }
+
+    public function actionGet($omsId = false) {
+        if(!$omsId) {
+            // Get all directions
+        } else {
+            $patientPerOms = Patient::model()->find('oms_id = :oms_id', array(':oms_id' => $omsId));
+            if(!$patientPerOms) {
+                if(Yii::app()->request->getIsAjaxRequest()) {
+                    echo CJSON::encode(array(
+                        'success' => true,
+                        'directions' => array()
+                    ));
+                    exit();
+                } else {
+                    throw new Exception('Пациент с ОМС ID = ' . $omsId . ' не найден ');
+                }
+            }
+            $directions = MDirection::model()->findAllPerPatientId($patientPerOms->id);
+            echo CJSON::encode(array(
+                'success' => true,
+                'directions' => $directions
+            ));
+        }
+    }
+}
+
+?>
