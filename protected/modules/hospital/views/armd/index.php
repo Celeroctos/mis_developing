2c75a22a2b33020c9629cde1244176f8be88fe0c
hushker <hushker@gmail.com>
2015-08-26 14:43:06 +0300

    http://office.avkcom.ru/trac/new/ticket/14415

--- protected/modules/hospital/views/armd/index.php
+++ protected/modules/hospital/views/armd/index.php
@@ -144,7 +144,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Киселева Ульяна Алексеевна</h4>
+        <h4 class="modal-title" id="myModalLabel">Киселева Ульяна Алексеевна (№10307/14, 42 года, гестационный сахарный диабет) </h4>
       </div>
       <div class="modal-body">
 
